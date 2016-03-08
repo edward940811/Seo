3b2181e81077b2a1c64fde43394c43b4a2285f16
schwannden <schwannden@gmail.com>
2016-03-08 21:57:31 +0800

    add hint

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -46,6 +46,9 @@ class ProjectsController < ApplicationController
   def update
     respond_to do |format|
       if @project.update(project_params)
+        User.find(params[:owners]).each do |user| #optimize this line
+          UserProject.create user: user, project: @project
+        end
         format.html { redirect_to @project, notice: 'Project was successfully updated.' }
         format.json { render :show, status: :ok, location: @project }
       else
